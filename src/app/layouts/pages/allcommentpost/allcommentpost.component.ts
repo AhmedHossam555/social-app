46b732be4763788172977ec0cc04b1cb19049ccf
ahmed hossam <ahmed.hossam.elgamal77@gmail.com>
2024-09-22 23:29:01 +0300

    comments  with  all post

--- src/app/layouts/pages/allcommentpost/allcommentpost.component.ts
+++ src/app/layouts/pages/allcommentpost/allcommentpost.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, input, Input, InputSignal, OnChanges, OnInit, signal, WritableSignal } from '@angular/core';
+import { CommentsService } from '../../../core/services/comments/comments.service';
+import { Comment } from '../../../core/interfaces/posts';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-allcommentpost',
   standalone: true,
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './allcommentpost.component.html',
   styleUrl: './allcommentpost.component.scss'
 })
-export class AllcommentpostComponent {
+export class AllcommentpostComponent implements OnChanges {
+  Id:InputSignal<string> = input('')
+  comments:WritableSignal<Comment[]> = signal([]);
+  constructor(private _comments: CommentsService){
 
+  }
+ 
+  ngOnChanges(){
+    this._comments.getPostsComment(this.Id()).subscribe({
+      next:(res)=>{
+        this.comments.set(res.comments)
+      }
+    })
+  }
+  
 }
