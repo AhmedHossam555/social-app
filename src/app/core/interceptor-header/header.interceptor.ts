326ef4f0d86b018b520a46fc67cab9d9b4f93fd9
ahmed hossam <ahmed.hossam.elgamal77@gmail.com>
2024-09-22 15:29:29 +0300

    interceptor header created

---
+++ src/app/core/interceptor-header/header.interceptor.ts
@@ -0,0 +1,17 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { PlatformService } from '../services/plat/platform.service';
+
+export const headerInterceptor: HttpInterceptorFn = (req, next) => {
+  let _plat = inject(PlatformService);
+
+  if(req.url.includes('.com/posts') || req.url.includes('.com/comments')){
+    if(_plat.getPlatfromId()){
+      let token:any = {token:window.localStorage.getItem('userToken')};
+      req = req.clone({
+        setHeaders:token,
+        })
+    }
+  }
+  return next(req);
+};
