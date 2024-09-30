882ddcf1f9e355b8e9267332a6447f928fd4f266
ahmed hossam <ahmed.hossam.elgamal77@gmail.com>
2024-10-01 00:31:07 +0300

    update app.config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -6,9 +6,10 @@ import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { headerInterceptor } from './core/interceptor-header/header.interceptor';
 import { loadingInterceptor } from './core/interceptor-loading/loading.interceptor';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(),
+  providers: [provideRouter(routes), provideClientHydration(),importProvidersFrom(BrowserAnimationsModule),
     provideHttpClient(withFetch(),withInterceptors([headerInterceptor, loadingInterceptor]))
   ]
 };
