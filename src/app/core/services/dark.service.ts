bc305b9fcbb7a527acb3f7be0104e0804bb9cb8d
ahmed hossam <ahmed.hossam.elgamal77@gmail.com>
2024-10-02 12:18:24 +0300

    update dark mode in localstorage

--- src/app/core/services/dark.service.ts
+++ src/app/core/services/dark.service.ts
@@ -1,12 +1,22 @@
-import { Injectable, signal, WritableSignal } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { effect, inject, Injectable, PLATFORM_ID, signal } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DarkService {
-  darkmode:WritableSignal<string> = signal('null')
+  x = inject(PLATFORM_ID)
+  darkmode= signal<string>(isPlatformBrowser(this.x)?JSON.parse(window.localStorage.getItem('darkmode')??'null'):'');
 
-  constructor() { }
+  constructor() { 
+    effect(()=>{
+      if(isPlatformBrowser(this.x)){
+        window.localStorage.setItem('darkmode', JSON.stringify(this.darkmode()))
+      }
+    })
+  }
+  
+ 
   updateMode(){
     this.darkmode.update((val)=>val === 'dark'?'null':'dark');
   }
